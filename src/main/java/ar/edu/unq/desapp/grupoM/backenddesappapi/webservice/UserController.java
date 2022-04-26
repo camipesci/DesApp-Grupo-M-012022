c05126869f90d0369cfd742fc6b37c2e396eca6a
cami1 <camila.pesci.a@gmail.com>
2022-04-26 20:24:35 -0300

    Add get, put y delete from user and refactor userController

--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/webservice/UserController.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/webservice/UserController.java
@@ -1,42 +1,71 @@
 package ar.edu.unq.desapp.grupoM.backenddesappapi.webservice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import ar.edu.unq.desapp.grupoM.backenddesappapi.model.exceptions.InvalidEmailException;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ar.edu.unq.desapp.grupoM.backenddesappapi.model.User;
-//import ar.edu.unq.desapp.grupoM.backenddesappapi.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @EnableAutoConfiguration
 public class UserController {
-    User last_user = null;
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+    List<User> users = new ArrayList<User>();
+
 
     @PostMapping("/api/createUser")
     @ResponseBody
     public ResponseEntity<User> createUser(@RequestBody User newUser) {
          User user = newUser;
-         last_user = user;
+         users.add(user);
         return ResponseEntity.ok().body(user);
     }
 
     @GetMapping("/api/allUsers")
-    public ResponseEntity<User> allUsers() {
+    public ResponseEntity<List<User>> allUsers() {
 
-        return ResponseEntity.ok().body(last_user);
+        return ResponseEntity.ok().body(users);
     }
 
+    @DeleteMapping("/api/deleteUser/{wallet}")
+    public ResponseEntity<List<User>>  deleteUser(@PathVariable Integer wallet) {
 
-    @RequestMapping(value = "/api/version", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<?> getVersion() {
-        String version = "0.2.2";
-        Map<String, String> resultado = new HashMap<String, String>();
-        resultado.put("version", version);
-        return ResponseEntity.ok().body(resultado);
+        users.removeIf(u -> u.getWallet().equals(wallet));
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/api/getUser/{wallet}")
+    public ResponseEntity<Optional<User>> getUser(@PathVariable Integer wallet) {
+
+        Optional<User> user = users.stream().
+                filter(u -> u.getWallet().equals(wallet)).
+                findFirst();
+
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("/api/updateUser/{wallet}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer wallet, @RequestBody User userUpdate) {
+
+        User newUser = userUpdate;
+        try {
+            users.add(newUser);
+            //Remove all User
+            users.removeIf(u -> u.getWallet().equals(wallet));
+        }
+        catch(InvalidEmailException e ){
+            logger.info("Error delete user " + e.getMessage());
+        }
+
+        return ResponseEntity.ok().body(newUser);
     }
 
 }
