bbd4c60fa453c47103dc6bb8980f433b2f631ad3
Nicolas <rodriguez.n@outlook.com>
2022-04-27 20:01:35 -0300

    Add User service and refactor UserController, also attempt to run test when running circle ci

--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/service/UserService.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/service/UserService.java
@@ -1,32 +1,59 @@
-/*package ar.edu.unq.desapp.grupoM.backenddesappapi.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
+package ar.edu.unq.desapp.grupoM.backenddesappapi.service;
 import ar.edu.unq.desapp.grupoM.backenddesappapi.model.User;
-import ar.edu.unq.desapp.grupoM.backenddesappapi.repositories.UserRepository;
+import ar.edu.unq.desapp.grupoM.backenddesappapi.model.exceptions.InvalidEmailException;
+import ar.edu.unq.desapp.grupoM.backenddesappapi.webservice.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Service
 public class UserService {
+    List<User> users = new ArrayList<User>();
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @Autowired
-    private UserRepository repository;
-
-    @Transactional
-    public User save(User model) {
-        return this.repository.save(model);
+    public  List<User> allUsers() {
+        return this.users;
     }
 
-    public User findByID(Integer id) {
-        return this.repository.findById(id).get();
+    public User create_new_user(User newUser) {
+        User user = newUser;
+        users.add(user);
+        return user;
     }
 
-    @Transactional
-    public List<User> findAll() {
-        return this.repository.findAll();
+    public List<User> deleteUser(@PathVariable Integer user_wallet) {
+        allUsers().removeIf(u -> u.getWallet().equals(user_wallet));
+         return allUsers();
+    }
+
+
+    public User getUser(@PathVariable Integer wallet) {
+        User user = allUsers().stream().filter(u -> u.getWallet().equals(wallet)).findFirst().get();
+        return user;
+    }
+
+    public User updateUser(Integer user_wallet, User userUpdate) {
+
+        User newUser = userUpdate;
+        try {
+            allUsers().add(newUser);
+            //Remove all User
+            allUsers().removeIf(u -> u.getWallet().equals(user_wallet));
+        }
+        catch(InvalidEmailException e ){
+            logger.info("Error delete user " + e.getMessage());
+        }
+
+        return newUser;
     }
 }
-*/
--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/webservice/UserController.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/webservice/UserController.java
@@ -2,70 +2,47 @@ package ar.edu.unq.desapp.grupoM.backenddesappapi.webservice;
 
 import java.util.*;
 
-import ar.edu.unq.desapp.grupoM.backenddesappapi.model.exceptions.InvalidEmailException;
+import ar.edu.unq.desapp.grupoM.backenddesappapi.service.UserService;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ar.edu.unq.desapp.grupoM.backenddesappapi.model.User;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 @RestController
 @EnableAutoConfiguration
 public class UserController {
-    Logger logger = LoggerFactory.getLogger(UserController.class);
-    List<User> users = new ArrayList<User>();
-
+   UserService userService = new UserService();
 
     @PostMapping("/api/createUser")
     @ResponseBody
     public ResponseEntity<User> createUser(@RequestBody User newUser) {
-         User user = newUser;
-         users.add(user);
+        User user = userService.create_new_user(newUser);
         return ResponseEntity.ok().body(user);
     }
 
     @GetMapping("/api/allUsers")
     public ResponseEntity<List<User>> allUsers() {
-
+        List<User> users = userService.allUsers();
         return ResponseEntity.ok().body(users);
     }
 
-    @DeleteMapping("/api/deleteUser/{wallet}")
-    public ResponseEntity<List<User>>  deleteUser(@PathVariable Integer wallet) {
-
-        users.removeIf(u -> u.getWallet().equals(wallet));
-
+    @DeleteMapping("/api/deleteUser/{user_wallet}")
+    public ResponseEntity<List<User>>  deleteUser(@PathVariable Integer user_wallet) {
+        List<User> users = userService.deleteUser(user_wallet);
         return ResponseEntity.ok().body(users);
     }
 
-    @GetMapping("/api/getUser/{wallet}")
-    public ResponseEntity<Optional<User>> getUser(@PathVariable Integer wallet) {
-
-        Optional<User> user = users.stream().
-                filter(u -> u.getWallet().equals(wallet)).
-                findFirst();
-
+    @GetMapping("/api/getUser/{user_wallet}")
+    public ResponseEntity<User> getUser(@PathVariable Integer user_wallet) {
+        User user = userService.getUser(user_wallet);
         return ResponseEntity.ok().body(user);
     }
 
-    @PutMapping("/api/updateUser/{wallet}")
-    public ResponseEntity<User> updateUser(@PathVariable Integer wallet, @RequestBody User userUpdate) {
-
-        User newUser = userUpdate;
-        try {
-            users.add(newUser);
-            //Remove all User
-            users.removeIf(u -> u.getWallet().equals(wallet));
-        }
-        catch(InvalidEmailException e ){
-            logger.info("Error delete user " + e.getMessage());
-        }
-
+    @PutMapping("/api/updateUser/{user_wallet}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer user_wallet, @RequestBody User userUpdate) {
+        User newUser = userService.updateUser(user_wallet, userUpdate);
         return ResponseEntity.ok().body(newUser);
     }
-
 }
