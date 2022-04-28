9555833be2d753efd03efeed100ebfc67a66b470
cami1 <camila.pesci.a@gmail.com>
2022-04-28 20:28:41 -0300

    Move UserTest to another path because CI didnt recogniseit

---
+++ src/test/java/ar/edu/unq/desapp/grupoM/backenddesappapi/UserTestCI.java
@@ -0,0 +1,134 @@
+package ar.edu.unq.desapp.grupoM.backenddesappapi;
+
+import ar.edu.unq.desapp.grupoM.backenddesappapi.builders.UserBuilder;
+import ar.edu.unq.desapp.grupoM.backenddesappapi.model.User;
+import ar.edu.unq.desapp.grupoM.backenddesappapi.model.exceptions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class UserTestCI {
+
+    //Valid User Creation
+    @Test
+    public void UserWithName() {
+        User ricardo = UserBuilder.user().withName("Ricardo").build();
+        assertEquals(ricardo.getName(), "Ricardo");
+    }
+
+    @Test
+    public void UserWithLastname() {
+        User ricardo = UserBuilder.user().withLastname("Fort").build();
+        assertEquals(ricardo.getLastName(), "Fort");
+    }
+
+    @Test
+    public void UserWithAddress() {
+        User ricardo = UserBuilder.user().withAddress("Av. Mitre 1030").build();
+        assertEquals(ricardo.getAddress(), "Av. Mitre 1030");
+    }
+
+    @Test
+    public void UserWithEmail() {
+        User ricardo = UserBuilder.user().withEmail("ricardoFort@gmail.com").build();
+        assertEquals(ricardo.getEmail(), "ricardoFort@gmail.com");
+    }
+
+     @Test
+    public void UserWithCvu() {
+        BigInteger cvu = new BigInteger("1234567890987654321234");
+        User ricardo = UserBuilder.user().withCvu(cvu).build();
+        assertEquals(ricardo.getCvu(), cvu);
+    }
+
+    @Test
+    public void UserWithWallet() {
+        User ricardo = UserBuilder.user().withWallet(12345678).build();
+        assertEquals(ricardo.getWallet(), 12345678);
+    }
+
+    @Test
+    public void UserWithPassword() {
+        User ricardo = UserBuilder.user().withPassword("Ricardo123@").build();
+        assertEquals(ricardo.getPassword(), "Ricardo123@");
+    }
+
+    // InValid User Creation
+
+    @Test
+    public void UserCannotHaveAnEmptyName(){
+        assertThrows(InvalidNameException.class , ()->  UserBuilder.user().withName("").build() );
+    }
+
+    @Test
+    public void UserCannotHaveAnEmptyLastname(){
+        assertThrows(InvalidLastNameException.class , ()->  UserBuilder.user().withLastname("").build() );
+    }
+
+    @Test
+    public void UserCannotHaveAnEmptyEmail(){
+        assertThrows(InvalidEmailException.class , ()->  UserBuilder.user().withEmail("").build() );
+    }
+
+    @Test
+    public void UserCannotHaveAnEmptyAddress(){
+        assertThrows(InvalidAddressException.class , ()->  UserBuilder.user().withAddress("").build() );
+    }
+
+    @Test
+    public void UserCannotHaveAnEmptyPassword(){
+        assertThrows(InvalidPasswordException.class , ()->  UserBuilder.user().withPassword("").build() );
+    }
+
+    @Test
+    public void UserWithInvalidCvu(){
+        BigInteger cvu = new BigInteger("123");
+        assertThrows(InvalidCvuException.class , ()->  UserBuilder.user().withCvu(cvu).build() );
+    }
+
+    @Test
+    public void UserWithAShortName(){
+        assertThrows(InvalidNameException.class , ()->  UserBuilder.user().withName("a").build() );
+    }
+
+    @Test
+    public void UserWithAShortLastname(){
+        assertThrows(InvalidLastNameException.class , ()->  UserBuilder.user().withLastname("b").build() );
+    }
+
+    @Test
+    public void UserWithInvalidEmail(){
+        assertThrows(InvalidEmailException.class , ()->  UserBuilder.user().withEmail("a").build() );
+    }
+
+    @Test
+    public void UserWithInvalidAddress(){
+        assertThrows(InvalidAddressException.class , ()->  UserBuilder.user().withAddress("a").build() );
+    }
+
+    @Test
+    public void UserWithInvalidPassword(){
+        assertThrows(InvalidPasswordException.class , ()->  UserBuilder.user().withPassword("123").build() );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
