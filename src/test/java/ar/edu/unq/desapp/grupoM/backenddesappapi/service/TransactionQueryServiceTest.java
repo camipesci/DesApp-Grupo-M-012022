ccdab0c40c719ae525fb39116d5b67d284e4d001
cami1 <camila.pesci.a@gmail.com>
2022-05-11 15:05:48 -0300

    Add createTransaction in TransactionQueryService, refactor TestserviceHelper and add TransactionQueryServiceTest

---
+++ src/test/java/ar/edu/unq/desapp/grupoM/backenddesappapi/service/TransactionQueryServiceTest.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.desapp.grupoM.backenddesappapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TransactionQueryServiceTest extends TestServiceHelper {
+
+    @Test
+    void getTransactions() {
+       assertEquals(transactionQueryService.getTransactions().get(0).getCryptoCurrency(), transaction.getCryptoCurrency());
+       assertEquals(transactionQueryService.getTransactions().size(), 2);
+    }
+
+    @Test
+    void findTransaction() throws Exception {
+        assertEquals(transactionQueryService.findTransaction(h2_transaction.id).getUserId(), h2_transaction.getUserId());
+        assertEquals(transactionQueryService.getTransactions().size(), 1);
+    }
+}
