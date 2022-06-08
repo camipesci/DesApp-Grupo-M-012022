7e48d211e5ff19b936fafa6f4e22da2353594712
Nicolas <rodriguez.n@outlook.com>
2022-06-08 19:23:09 -0300

    Refactor USD Controller, add security and hide token, and add comments

--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/controller/USDPriceController.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/controller/USDPriceController.java
@@ -3,43 +3,44 @@ package ar.edu.unq.desapp.grupoM.backenddesappapi.controller;
 import lombok.NoArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
-
 @RestController
 @NoArgsConstructor
 public class USDPriceController {
-
     public Double getUSDPriceDouble() throws IOException {
+        // Setup url and credentials to thi usd api
         URL binance_url = new URL("https://api.estadisticasbcra.com/usd_of");
         HttpURLConnection http = (HttpURLConnection)binance_url.openConnection();
         http.setRequestProperty("Accept", "application/json");
-        //TO DO: move token to another file
-        http.setRequestProperty("Authorization", "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODU2NjA0MDYsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJjYW1pbGEucGVzY2kuYUBnbWFpbC5jb20ifQ.F-5_ZVANljRvZ3qdfA9R-P2LH0PiuBJmOJOkmMJoPDiFeShlVK_FiQ0UDCOrRAthBKER2B0y98jGH_QibkUwQw");
+        http.setRequestProperty("Authorization", "Bearer " + System.getenv("USD_TOKEN"));
 
+        // Proccess USD API Response
         String response = this.getResponseBody(http);
+
+        // Get array of dolar prices
         JSONArray usd_prices_array = new JSONArray(response);
 
-        JSONObject usd_price = (JSONObject) usd_prices_array.get(usd_prices_array.length() -1);
-        Double price_value = usd_price.getDouble("v");
-        return price_value;
+        // Get newest dolar price (last element)
+        JSONObject today_usd_price = (JSONObject) usd_prices_array.get(usd_prices_array.length() -1);
 
+        // Get the dolar price value
+        Double usd_price_value = today_usd_price.getDouble("v");
+
+        return usd_price_value;
     }
 
     public static String getResponseBody(HttpURLConnection conn) {
+        // This is a method that get the URLConnection responses to a legible format
         BufferedReader br = null;
         StringBuilder body = null;
         String line = "";
-        try {//  w  w w .j  a  v a 2s . c  o m
+        try {
             br = new BufferedReader(new InputStreamReader(
                     conn.getInputStream()));
             body = new StringBuilder();
@@ -50,5 +51,4 @@ public class USDPriceController {
             throw new RuntimeException(e);
         }
     }
-
 }
