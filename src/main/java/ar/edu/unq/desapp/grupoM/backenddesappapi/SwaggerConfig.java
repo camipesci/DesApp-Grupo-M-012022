6e2d817351da9127f06a0f066fff2798fc765fb4
Nicolas <rodriguez.n@outlook.com>
2022-06-12 18:46:51 -0300

    Add swagger UI docs

--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/SwaggerConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/SwaggerConfig.java
@@ -3,6 +3,7 @@ package ar.edu.unq.desapp.grupoM.backenddesappapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,8 +17,12 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("ar.edu.unq.desapp.grupoL.backenddesappapi"))
+                .apis(RequestHandlerSelectors.basePackage("ar.edu.unq.desapp.grupoM.backenddesappapi"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
