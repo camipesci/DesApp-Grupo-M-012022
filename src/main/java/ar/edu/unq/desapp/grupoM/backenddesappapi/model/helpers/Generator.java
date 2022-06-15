c21bd29fab28fef74146a4438e93770a6aa5aa88
Nicolas <rodriguez.n@outlook.com>
2022-06-15 17:24:21 -0300

    Fix Sonar cloud issues

--- src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/model/helpers/Generator.java
+++ src/main/java/ar/edu/unq/desapp/grupoM/backenddesappapi/model/helpers/Generator.java
@@ -1,9 +1,10 @@
 package ar.edu.unq.desapp.grupoM.backenddesappapi.model.helpers;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class Generator {
-    private Random rnd = new Random();
+    private SecureRandom rnd = new SecureRandom();
     public  String generateCVU() {
         String chars = "0123456789";
         Integer cvu_len = 22;
