1f8c166998b32d755df66b39e41cd202bb338f8e
Nicolas <rodriguez.n@outlook.com>
2022-06-29 17:42:51 -0300

    Add token bcra to app properties

--- src/test/java/ar/edu/unq/desapp/grupoM/backenddesappapi/service/UserServiceUser.java
+++ src/test/java/ar/edu/unq/desapp/grupoM/backenddesappapi/service/UserServiceUser.java
@@ -1,13 +1,9 @@
 package ar.edu.unq.desapp.grupoM.backenddesappapi.service;
+
 import ar.edu.unq.desapp.grupoM.backenddesappapi.model.User;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserServiceUser extends UserServiceHelper {
 
